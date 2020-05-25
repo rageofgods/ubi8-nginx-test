65efa97cd8529f5e82e3121a2451738d5f426ddd
Eugene <rageofgods@gmail.com>
2020-05-25 13:27:19 +0300

    library

---
+++ var/ext_test_pipeline.groovy
@@ -0,0 +1,111 @@
+def call(Map pipelineParams) {
+    pipeline {
+        agent {
+            label pipelineParams.deploy_env
+        }
+        environment {
+            DOCKERFILE_NAME = pipelineParams.dockerfile_name
+            TEMPLATE_NAME = pipelineParams.template_name
+            DOCKER_REGISTRY = pipelineParams.docker_registry
+            DOCKER_REGISTRY_AUTH_ID = pipelineParams.docker_registry_auth_id
+            BUILD_NAME = pipelineParams.build_name
+            RH_REGISTRY = pipelineParams.rh_registry
+            RH_REGISTRY_AUTH_ID = pipelineParams.rh_registry_auth_id
+            OCP_URL = pipelineParams.ocp_url
+            OCP_AUTH_ID = pipelineParams.ocp_auth_id
+            IMAGESTREAM = pipelineParams.imagestream
+            GIT_COMMIT_SHORT = sh(
+                script: "printf \$(git rev-parse --short ${GIT_COMMIT})",
+                returnStdout: true
+            )
+        }
+        
+        stages {
+            stage("Docker registry login") {
+                steps {
+                    echo "=====docker login registry====="
+                    withCredentials([usernamePassword(credentialsId: "$DOCKER_REGISTRY_AUTH_ID", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                        sh """
+                        docker login $RH_REGISTRY -u $USERNAME -p $PASSWORD
+                        docker login $DOCKER_REGISTRY -u $USERNAME -p $PASSWORD
+                        """
+                    }
+                }
+            }
+            stage("Docker build image") {
+                steps {
+                    echo "=====docker login and build====="
+                    sh """
+                    docker build -t $DOCKER_REGISTRY/$BUILD_NAME:$GIT_COMMIT_SHORT -f $DOCKERFILE_NAME .
+                    """
+                }
+            }
+            stage("Docker push image") {
+                steps {
+                    echo "=====docker login and push====="
+                    sh """
+                    docker push $DOCKER_REGISTRY/$BUILD_NAME:$GIT_COMMIT_SHORT
+                    """
+                }
+            }
+            stage("ocp login") {
+                steps {
+                    echo "=====ocp login====="
+                    withCredentials([string(credentialsId: "$OCP_AUTH_ID", variable: 'TOKEN')]) {
+                        sh """
+                        oc login $OCP_URL --token $TOKEN
+                        """
+                    }
+                }
+            }
+            stage("ocp init image stream and teamplate") {
+                steps {
+                    echo "=====ocp login====="
+                        sh """
+                        oc apply -f $IMAGESTREAM
+                        oc apply -f $TEMPLATE_NAME
+                        """
+                }
+            }
+            stage("ocp deploy") {
+                steps {
+                    echo "=====ocp tag new image as latest====="
+                        sh """
+                        oc tag $DOCKER_REGISTRY/$BUILD_NAME:$GIT_COMMIT_SHORT $BUILD_NAME:latest
+                        """
+                }
+            }
+            stage("ocp create new app from the template") {
+                when {
+                    not {
+                        expression {
+                            def status = sh(script: "oc get all --selector app=$BUILD_NAME | grep Running", returnStatus: true) 
+                            if (status != 0) {
+                                return false;
+                            }
+                            else {
+                                return true;
+                            }
+                        }
+                    }
+                }
+                steps {
+                    echo "=====ocp create application====="
+                        sh """
+                        oc new-app --template=$BUILD_NAME
+                        """
+                }
+            }
+            stage("ocp get status") {
+                steps {
+                    echo "=====Waiting 15 second to build process catch-up====="
+                    sleep(time:15,unit:"SECONDS")
+                    echo "=====ocp get podes====="
+                        sh """
+                        oc get pods | grep Running | grep '1/1'
+                        """
+                }
+            }
+        }
+    }
+}
